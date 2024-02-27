434ad6a322b7044149c3e14e4ec9e220b66de526
Cody Johns <cjohns365@gmail.com>
2024-02-27 10:57:58 -0700

    Stop nave menu from being fixed position

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,7 +2,7 @@
 const Menu = () => {
 
     return (
-        <div className={"menu primary-color"}>
+        <div>
             <div className={"d-flex justify-content-center"}>
                 <div className={"d-flex pt-2 pb-2"}>
                     <img alt={"Regex-GPT"} src={require("../assets/regex-gpt.png")} className={"logo"} />
